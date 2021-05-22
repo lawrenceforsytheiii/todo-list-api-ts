124523e2b289f4cf3f7c0cc1c65ff8645cf78ab0
lawrenceforsytheiii <lawrence@whispir.com>
2021-05-22 11:49:45 -0700

    parsed out dynamodb resources into separate resource file

--- serverless.ts
+++ serverless.ts
@@ -1,6 +1,6 @@
 import type { AWS } from '@serverless/typescript';
-
 import hello from '@functions/hello';
+import dynamodbTables from './resources/dynamodb';
 
 const serverlessConfiguration: AWS = {
   service: 'todo-list-api-ts',
@@ -78,44 +78,7 @@ const serverlessConfiguration: AWS = {
     individually: true,
   },
   resources: {
-    Resources: {
-      ListTable: {
-        Type: 'AWS::DynamoDB::Table',
-        DeletionPolicy: 'Retain',
-        Properties: {
-          TableName: '${self:provider.environment.LIST_TABLE}',
-          AttributeDefinitions: [
-            { AttributeName: 'id', AttributeType: 'S' }
-          ],
-          KeySchema: [
-            { AttributeName: 'id', KeyType: 'HASH' }
-          ],
-          ProvisionedThroughput: {
-            ReadCapacityUnits: '${self:custom.TABLE_THROUGHPUT}',
-            WriteCapacityUnits: '${self:custom.TABLE_THROUGHPUT}'
-          }
-        }
-      },
-      TaskTable: {
-        Type: 'AWS::DynamoDB::Table',
-        DeletionPolicy: 'Retain',
-        Properties: {
-          TableName: '${self:provider.environment.TASK_TABLE}',
-          AttributeDefinitions: [
-            { AttributeName: 'id', AttributeType: 'S' },
-            { AttributeName: 'listId', AttributeType: 'S' }
-          ],
-          KeySchema: [
-            { AttributeName: 'id', KeyType: 'HASH' },
-            { AttributeName: 'listId', KeyType: 'RANGE' }
-          ],
-          ProvisionedThroughput: {
-            ReadCapacityUnits: '${self:custom.TABLE_THROUGHPUT}',
-            WriteCapacityUnits: '${self:custom.TABLE_THROUGHPUT}'
-          }
-        }
-      }
-    }
+    Resources: dynamodbTables,
   }
 };
 
